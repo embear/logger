ebe32faa5ab30129d75197bef9dfa8af465f440b
Markus Braun <markus.braun@krawel.de>
2013-10-08 09:59:02 +0200

    preserve spaces using html tags

--- test/main.c
+++ test/main.c
@@ -893,17 +893,17 @@ int main(int  argc,
   id = logger_id_request("logger_test_id");
   assert(LOGGER_OK == logger_id_enable(id));
   assert(LOGGER_OK == logger_id_level_set(id, LOGGER_DEBUG));
-  assert(LOGGER_OK == logger_id_color_string_set(id, "<span style=\"font-family:Courier;color:#00FF00\">", "</span>"));
+  assert(LOGGER_OK == logger_id_color_string_set(id, "<span style=\"font-family:Courier;color:#00FF00;white-space:pre\">", "</span>"));
   assert(LOGGER_OK == logger_color_message_enable());
-  assert(LOGGER_OK == logger_color_prefix_string_set(LOGGER_UNKNOWN, "<span style=\"font-family:Courier;color:#FFFFFF;background-color:#000000\">", "</span>"));
-  assert(LOGGER_OK == logger_color_prefix_string_set(LOGGER_DEBUG,   "<span style=\"font-family:Courier;color:#FFFFFF;background-color:#000000\">", "</span>"));
-  assert(LOGGER_OK == logger_color_prefix_string_set(LOGGER_INFO,    "<span style=\"font-family:Courier;color:#000000;background-color:#FFFFFF\">", "</span>"));
-  assert(LOGGER_OK == logger_color_prefix_string_set(LOGGER_NOTICE,  "<span style=\"font-family:Courier;color:#000000;background-color:#0000FF\">", "</span>"));
-  assert(LOGGER_OK == logger_color_prefix_string_set(LOGGER_WARNING, "<span style=\"font-family:Courier;color:#000000;background-color:#00FFFF\">", "</span>"));
-  assert(LOGGER_OK == logger_color_prefix_string_set(LOGGER_ERR,     "<span style=\"font-family:Courier;color:#000000;background-color:#00FF00\">", "</span>"));
-  assert(LOGGER_OK == logger_color_prefix_string_set(LOGGER_CRIT,    "<span style=\"font-family:Courier;color:#000000;background-color:#FFFF00\">", "</span>"));
-  assert(LOGGER_OK == logger_color_prefix_string_set(LOGGER_ALERT,   "<span style=\"font-family:Courier;color:#000000;background-color:#FF00FF\">", "</span>"));
-  assert(LOGGER_OK == logger_color_prefix_string_set(LOGGER_EMERG,   "<span style=\"font-family:Courier;color:#000000;background-color:#FF0000\">", "</span>"));
+  assert(LOGGER_OK == logger_color_prefix_string_set(LOGGER_UNKNOWN, "<span style=\"font-family:Courier;color:#FFFFFF;background-color:#000000;white-space:pre\">", "</span>"));
+  assert(LOGGER_OK == logger_color_prefix_string_set(LOGGER_DEBUG,   "<span style=\"font-family:Courier;color:#FFFFFF;background-color:#000000;white-space:pre\">", "</span>"));
+  assert(LOGGER_OK == logger_color_prefix_string_set(LOGGER_INFO,    "<span style=\"font-family:Courier;color:#000000;background-color:#FFFFFF;white-space:pre\">", "</span>"));
+  assert(LOGGER_OK == logger_color_prefix_string_set(LOGGER_NOTICE,  "<span style=\"font-family:Courier;color:#000000;background-color:#0000FF;white-space:pre\">", "</span>"));
+  assert(LOGGER_OK == logger_color_prefix_string_set(LOGGER_WARNING, "<span style=\"font-family:Courier;color:#000000;background-color:#00FFFF;white-space:pre\">", "</span>"));
+  assert(LOGGER_OK == logger_color_prefix_string_set(LOGGER_ERR,     "<span style=\"font-family:Courier;color:#000000;background-color:#00FF00;white-space:pre\">", "</span>"));
+  assert(LOGGER_OK == logger_color_prefix_string_set(LOGGER_CRIT,    "<span style=\"font-family:Courier;color:#000000;background-color:#FFFF00;white-space:pre\">", "</span>"));
+  assert(LOGGER_OK == logger_color_prefix_string_set(LOGGER_ALERT,   "<span style=\"font-family:Courier;color:#000000;background-color:#FF00FF;white-space:pre\">", "</span>"));
+  assert(LOGGER_OK == logger_color_prefix_string_set(LOGGER_EMERG,   "<span style=\"font-family:Courier;color:#000000;background-color:#FF0000;white-space:pre\">", "</span>"));
 
   puts("All messages have the same color");
   assert(LOGGER_OK == logger(id, LOGGER_DEBUG,   "test %d - id %d - LOGGER_DEBUG   in line %d\n", test, id, __LINE__));
